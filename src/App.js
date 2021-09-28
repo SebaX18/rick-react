cb53bacfa789ef7412729e1311ab7ca00152dd53
SebaX18 <89862738+SebaX18@users.noreply.github.com>
2021-09-28 13:29:32 -0300

    Add files via upload
    Ez los pollos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Encabezado from "./components/Encabezado";
+import Main from "./components/Main";
+import'./App.css'
 
 const App = () => {
   return (
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.css';
+const Main = () => {
+    return   
+    <div>
+        <h1 class="prueba">
+          The Rick and Morty
+        </h1>
+    </div>;
+    
+  };
+  export default Main;
